56358a5656fdd08cf53f8172b922e9c067b6892d
Ariya Goudarzi <115103967+ariyagoudarzi@users.noreply.github.com>
2024-09-22 11:48:38 +0330

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,154 @@
+import { useEffect, useReducer } from "react";
+import Header from "./Header";
+import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./Screen";
+import Question from "./Question";
+import NextBtn from "./NextBtn";
+import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
+import ResetBtn from "./ResetBtn";
+import Timer from "./Timer";
+import { SECS_PER_QUESTION, QUESTION_API } from "./Config";
+
+const initialState = {
+  questions: [],
+  status: "loading",
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+  secondRemaining: null,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFaild":
+      return { ...state, status: "error" };
+    case "start":
+      return {
+        ...state,
+        status: "active",
+        secondRemaining: state.questions.length * SECS_PER_QUESTION,
+      };
+    case "newAnswer":
+      const question = state.questions.at(state.index);
+
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return { ...state, index: state.index + 1, answer: null };
+    case "finish":
+      return {
+        ...state,
+        status: "finished",
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    case "reset":
+      return {
+        ...state,
+        index: 0,
+        answer: null,
+        points: 0,
+        status: "ready",
+        secondRemaining: null,
+      };
+    case "tick":
+      return {
+        ...state,
+        secondRemaining: state.secondRemaining - 1,
+        status: state.secondRemaining === 0 ? "finished" : state.status,
+      };
+
+    default:
+      break;
+  }
+}
+
+function App() {
+  const [
+    { questions, status, index, answer, points, highscore, secondRemaining },
+    dispatch,
+  ] = useReducer(reducer, initialState);
+
+  const numQ = questions.length;
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0
+  );
+
+  useEffect(function () {
+    async function fetchData() {
+      try {
+        const res = await fetch(QUESTION_API);
+        const data = await res.json();
+
+        dispatch({ type: "dataReceived", payload: data });
+      } catch (err) {
+        dispatch({ type: "dataFaild", payload: err.message });
+        console.log(err.message);
+      }
+    }
+
+    fetchData();
+  }, []);
+
+  return (
+    <div className="app">
+      <Header />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && <StartScreen numQ={numQ} dispatch={dispatch} />}
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQ}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <footer>
+              <Timer dispatch={dispatch} secondRemaining={secondRemaining} />
+
+              <NextBtn
+                dispatch={dispatch}
+                answer={answer}
+                index={index}
+                numQ={numQ}
+              />
+            </footer>
+          </>
+        )}
+        {status === "finished" && (
+          <>
+            <FinishScreen
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              highscore={highscore}
+            />
+            <ResetBtn dispatch={dispatch} />
+          </>
+        )}
+      </Main>
+    </div>
+  );
+}
+
+export default App;
